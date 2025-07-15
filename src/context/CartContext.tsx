3eed40b560a8bf1b02f55e7f5e8486a0b6b9bbe8
stefangrobler155 <stefangrobler155@gmail.com>
2025-07-15 10:56:26 +0200

    refactor: remove unused Product import in CartContext

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { createContext, useState, useEffect, useContext, ReactNode } from 'react';
-import { Product } from '@/lib/types';
+
 
 // This is the shape of the cart data we expect back from the CoCart API
 type WooCart = {
